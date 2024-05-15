e1f3104961aa08017d3409a768d63cf54b358f5e
Minhtuan1235 <145755494+Minhtuan1235@users.noreply.github.com>
2024-05-15 09:10:54 +0700

    Add files via upload

---
+++ 2024802010507_TranMinhTuan_Bai3/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import TodoApp from "./Screens/TodoApp"
+
+const App= ()=> {
+  return (
+
+           <TodoApp />
+ 
+   
+  
+  );
+}
+export default App;
